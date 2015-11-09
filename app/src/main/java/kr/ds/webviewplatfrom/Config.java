5d5e28a79105c2e9d09c829e3ff892b35f320659
조동석 <lab2734@gmail.com>
2015-11-09 09:55:39 +0900

    2015.11.09

--- app/src/main/java/kr/ds/webviewplatfrom/Config.java
+++ app/src/main/java/kr/ds/webviewplatfrom/Config.java
@@ -1,6 +1,8 @@
 package kr.ds.webviewplatfrom;
 
-
+/**
+ * Created by Administrator on 2015-11-04.
+ */
 public class Config {
     public static String WEBPAGE = "http://www.pctu1213.cafe24.com";
 }
