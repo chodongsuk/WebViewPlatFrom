d2fbae69399fefafd85b9daa4572df76686e4b58
조동석 <lab2734@gmail.com>
2015-11-09 09:53:41 +0900

    2015.11.09

---
+++ app/src/main/java/kr/ds/webviewplatfrom/Config.java
@@ -0,0 +1,6 @@
+package kr.ds.webviewplatfrom;
+
+
+public class Config {
+    public static String WEBPAGE = "http://www.pctu1213.cafe24.com";
+}
--- app/src/main/java/kr/ds/webviewplatfrom/MainActivity.java
+++ app/src/main/java/kr/ds/webviewplatfrom/MainActivity.java
@@ -2,6 +2,7 @@ package kr.ds.webviewplatfrom;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -22,10 +23,17 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-    private WebView WebHomepage;
+    private WebView mWebView;
     private LinearLayout ProgressArea;
     private BackPressCloseHandler backPressCloseHandler = new BackPressCloseHandler(this);
-    private String Url = "http://www.kmhousing.com";
+    private String Url = Config.WEBPAGE;
+
+    public static final String INTENT_PROTOCOL_START = "intent:";
+    public static final String INTENT_PROTOCOL_INTENT = "#Intent;";
+    public static final String INTENT_PROTOCOL_END = ";end;";
+    public static final String GOOGLE_PLAY_STORE_PREFIX = "market://details?id=";
+
+
 
     public class BackPressCloseHandler {
 
@@ -42,12 +50,10 @@ public class MainActivity extends Activity {
                 showGuide();
                 return;
             }
-
             if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
                 activity.finish();
                 toast.cancel();
             }
-
         }
 
         private void showGuide() {
@@ -63,7 +69,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebHomepage = (WebView) findViewById(R.id.web_homepage);
+        mWebView = (WebView) findViewById(R.id.web_homepage);
         ProgressArea = (LinearLayout) findViewById(R.id.progress_area);
 
         try {
@@ -73,28 +79,24 @@ public class MainActivity extends Activity {
         }
     }
 
-
-
-
-
     public void getWebView(final String url) {
         final Handler handler = new Handler();
         Runnable doInit = new Runnable() {
             public void run() {
-                WebHomepage.getSettings().setLoadWithOverviewMode(true);// 축소된상태
-                WebHomepage.getSettings().setUseWideViewPort(true); // 뷰포트
-                WebHomepage.setVerticalScrollbarOverlay(true);
-                // WebHomepage.getSettings().setSupportZoom(true);// 줌컨트롤
-                // WebHomepage.getSettings().setBuiltInZoomControls(true);
-                WebHomepage.getSettings().setJavaScriptEnabled(true);
-                WebHomepage.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-                WebHomepage.getSettings().setSaveFormData(true);
-                WebHomepage.getSettings().setSavePassword(false);//해당비밀번호 창
-                WebHomepage.getSettings().setSupportMultipleWindows(false); // popup설정
-                WebHomepage.loadUrl(url);
-                WebHomepage.setWebViewClient(new WebViewClients());
+                mWebView.getSettings().setLoadWithOverviewMode(true);// 축소된상태
+                mWebView.getSettings().setUseWideViewPort(true); // 뷰포트
+                mWebView.setVerticalScrollbarOverlay(true);
+                // mWebView.getSettings().setSupportZoom(true);// 줌컨트롤
+                //mWebView.getSettings().setBuiltInZoomControls(true);
+                mWebView.getSettings().setJavaScriptEnabled(true);
+                mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                mWebView.getSettings().setSaveFormData(true);
+                //mWebView.getSettings().setSavePassword(false);//해당비밀번호 창
+                mWebView.getSettings().setSupportMultipleWindows(false); // popup설정
+                mWebView.loadUrl(url);
+                mWebView.setWebViewClient(new WebViewClients());
 
-                WebHomepage.setWebChromeClient(new WebChromeClient() {
+                mWebView.setWebChromeClient(new WebChromeClient() {
                     @Override
                     public boolean onJsAlert(WebView view, String url,
                                              String message, final JsResult result) {
@@ -110,8 +112,7 @@ public class MainActivity extends Activity {
                                         }).setCancelable(false).create().show();
                         return true;
 
-                    };
-
+                    }
 
 
                 });
@@ -124,33 +125,65 @@ public class MainActivity extends Activity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
-
-
-
     private class WebViewClients extends WebViewClient {
-
-        @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler,SslError error) {
-            super.onReceivedSslError(view, handler, error);
-            handler.proceed();
-        }
-
+//        @Override
+//        public void onReceivedSslError(WebView view, SslErrorHandler handler,SslError error) {
+//            super.onReceivedSslError(view, handler, error);
+//            handler.proceed();
+//        }
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url.startsWith("tel:")) {
-                Intent call_phone = new Intent(Intent.ACTION_CALL,	Uri.parse(url));
-                startActivity(call_phone);
-                return true;
-            }else if(url.startsWith("market:")){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-                return true;
+            //19버젼이후 체크 사항.
+            if (android.os.Build.VERSION.SDK_INT >= 19) {
+                if (url.startsWith(INTENT_PROTOCOL_START)) {
+
+                    final int customUrlStartIndex = INTENT_PROTOCOL_START.length();
+                    final int customUrlEndIndex = url.indexOf(INTENT_PROTOCOL_INTENT);
+                    if (customUrlEndIndex < 0) {
+                        return false;
+                    } else {
+                        final String customUrl = url.substring(customUrlStartIndex, customUrlEndIndex);
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        try {
+                            intent.setData(Uri.parse(customUrl));
+                            getBaseContext().startActivity(intent);
+                        } catch (ActivityNotFoundException e) {//액티비티가존재 하지 않을 경우.
+                            final int packageStartIndex = customUrlEndIndex+ INTENT_PROTOCOL_INTENT.length();
+                            final int packageEndIndex = url.indexOf(INTENT_PROTOCOL_END);
+
+                            final String packageName = url.substring(packageStartIndex,	packageEndIndex < 0 ? url.length()	: packageEndIndex);
+                            intent.setData(Uri.parse(GOOGLE_PLAY_STORE_PREFIX	+ packageName));
+                            getBaseContext().startActivity( intent );
+                        }
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
             }else{
-                view.loadUrl(url);
+                if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }else if (url.startsWith("mailto:")) {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }else if (url.startsWith("sms:")) {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }else if (!url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("javascript:") ) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }else{
+                    view.loadUrl(url);
+                }
             }
             return true;
         }
-
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             // TODO Auto-generated method stub
@@ -163,17 +196,21 @@ public class MainActivity extends Activity {
             ProgressArea.setVisibility(View.GONE);
         }
     }
-    public boolean onKeyDown(int KeyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            if (KeyCode == KeyEvent.KEYCODE_BACK) {
-                if(WebHomepage.canGoBack()){
-                    WebHomepage.goBack();
-                }else{
-                    backPressCloseHandler.onBackPressed();
-                }
-                return true;
-            }
-        }
-        return super.onKeyDown(KeyCode, event);
-    }
+//    @Override
+//    public boolean onKeyDown(int KeyCode, KeyEvent event) {
+//        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+//            if (KeyCode == KeyEvent.KEYCODE_BACK) {
+//                if(mWebView.canGoBack()){
+//                    mWebView.goBack();
+//                }else{
+//                    backPressCloseHandler.onBackPressed();
+//                }
+//                return true;
+//            }
+//        }
+//        return super.onKeyDown(KeyCode, event);
+//    }
+
+
+
 }
